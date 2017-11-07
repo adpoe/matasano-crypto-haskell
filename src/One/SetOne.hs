41bd4127ce4bb1e373e3eeb5cb78e25437f03c3e
Tony Poerio <tony@tonypoer.io>
2017-11-06 22:53:45 -0500

    Finished challenge #5

--- src/One/SetOne.hs
+++ src/One/SetOne.hs
@@ -100,3 +100,27 @@ challenge4 = Prelude.readFile "data/4.txt"
   . Data.List.lines
 -- SetOne Data.Ord Data.List Control.Monad> challenge4
 -- ("Now that the party is jumping\n",183.0)
+
+{- Challenge 5: Implement Repeating Key XOR -}
+poetry :: String
+poetry = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+
+repeatingKeyXor :: String -> String -> String
+repeatingKeyXor _ []  = []
+repeatingKeyXor key str = Char8.unpack (xorStrings keyPacked this) ++ repeatingKeyXor key rest
+  where
+    keyPacked = Char8.pack key
+    keyLen = Data.List.length key
+    this = (Char8.pack . Prelude.take keyLen) str
+    rest = Prelude.drop keyLen str
+
+repeatingKeyXor' :: String -> String -> ByteString
+repeatingKeyXor' key str = (Base16.encode . Char8.pack) (repeatingKeyXor key str)
+
+ch5output = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+challenge5 :: IO ()
+challenge5 = hspec $ do
+  describe "Challenge #5" $ do
+    it "Confirm repeatingKeyXor' is functional" $ do
+      (repeatingKeyXor' "ICE" poetry) `shouldBe` ch5output
